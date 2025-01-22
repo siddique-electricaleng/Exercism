c4ce9b5be2678d7560d2c97535b22ba60a2f3b31
siddique-electricaleng <ecsesiddique.297@gmail.com>
2025-01-22 16:00:20 +0600

    Office: Completed Sets

--- python/11. Sets/cater-waiter/sets.py
+++ python/11. Sets/cater-waiter/sets.py
@@ -21,8 +21,9 @@ def clean_ingredients(dish_name, dish_ingredients):
     followed by the de-duped `set` of ingredients as the second item.
     """
 
-    pass
+    return (dish_name, set(dish_ingredients))
 
+# print(clean_ingredients('Punjabi-Style Chole', ['onions', 'tomatoes', 'ginger paste', 'garlic paste', 'ginger paste', 'vegetable oil', 'bay leaves', 'cloves', 'cardamom', 'cilantro', 'peppercorns', 'cumin powder', 'chickpeas', 'coriander powder', 'red chili powder', 'ground turmeric', 'garam masala', 'chickpeas', 'ginger', 'cilantro']))
 
 def check_drinks(drink_name, drink_ingredients):
     """Append "Cocktail" (alcohol)  or "Mocktail" (no alcohol) to `drink_name`, based on `drink_ingredients`.
@@ -35,9 +36,9 @@ def check_drinks(drink_name, drink_ingredients):
     name followed by "Cocktail" (includes alcohol).
 
     """
+    return f"{drink_name} Mocktail" if set(drink_ingredients).isdisjoint(ALCOHOLS) else f"{drink_name} Cocktail"
 
-    pass
-
+# print(check_drinks('Shirley Tonic', ['cinnamon stick', 'scotch', 'whole cloves', 'ginger', 'pomegranate juice', 'sugar', 'club soda']))
 
 def categorize_dish(dish_name, dish_ingredients):
     """Categorize `dish_name` based on `dish_ingredients`.
@@ -52,7 +53,17 @@ def categorize_dish(dish_name, dish_ingredients):
 
     """
 
-    pass
+    categories = [VEGAN, VEGETARIAN, PALEO, KETO, OMNIVORE]
+    category_assign_name = {
+        0: "VEGAN",
+        1: "VEGETARIAN",
+        2: "PALEO",
+        3: "KETO",
+        4: "OMNIVORE"
+    }
+    for index, category in enumerate(categories):
+        if dish_ingredients.issubset(category):
+            return f"{dish_name}: {category_assign_name[index]}"
 
 
 def tag_special_ingredients(dish):
@@ -65,9 +76,10 @@ def tag_special_ingredients(dish):
     For the purposes of this exercise, all allergens or special ingredients that need to be tracked are in the
     SPECIAL_INGREDIENTS constant imported from `sets_categories_data.py`.
     """
+    # Just perform a set intersection to find which ingredients fall in the SPECIAL_INGREDIENTS and that auto returns a set
+    return (dish[0], set(dish[1]) & SPECIAL_INGREDIENTS)
 
-    pass
-
+# print(tag_special_ingredients(('Arugula and Roasted Pork Salad', ['pork tenderloin', 'arugula', 'pears', 'blue cheese', 'pine nuts', 'balsamic vinegar', 'onions', 'black pepper'])))
 
 def compile_ingredients(dishes):
     """Create a master list of ingredients.
@@ -77,9 +89,20 @@ def compile_ingredients(dishes):
 
     This function should return a `set` of all ingredients from all listed dishes.
     """
+    master_ingredients = set()
+    for dish in dishes:
+        master_ingredients = master_ingredients | dish
 
-    pass
+    return master_ingredients
 
+# Uncomment below to test and print the compile_ingredients function
+""" 
+dishes = [ {'tofu', 'soy sauce', 'ginger', 'corn starch', 'garlic', 'brown sugar', 'sesame seeds', 'lemon juice'},
+           {'pork tenderloin', 'arugula', 'pears', 'blue cheese', 'pine nuts',
+           'balsamic vinegar', 'onions', 'black pepper'},
+           {'honeydew', 'coconut water', 'mint leaves', 'lime juice', 'salt', 'english cucumber'}]
+ """
+# print(compile_ingredients(dishes))
 
 def separate_appetizers(dishes, appetizers):
     """Determine which `dishes` are designated `appetizers` and remove them.
@@ -92,8 +115,18 @@ def separate_appetizers(dishes, appetizers):
     Either list could contain duplicates and may require de-duping.
     """
 
-    pass
+    return list(set(dishes) - set(appetizers))
 
+""" 
+dishes =    ['Avocado Deviled Eggs','Flank Steak with Chimichurri and Asparagus', 'Kingfish Lettuce Cups',
+             'Grilled Flank Steak with Caesar Salad','Vegetarian Khoresh Bademjan','Avocado Deviled Eggs',
+             'Barley Risotto','Kingfish Lettuce Cups']
+          
+appetizers = ['Kingfish Lettuce Cups','Avocado Deviled Eggs','Satay Steak Skewers',
+              'Dahi Puri with Black Chickpeas','Avocado Deviled Eggs','Asparagus Puffs',
+              'Asparagus Puffs']
+ """
+# print(separate_appetizers(dishes, appetizers))
 
 def singleton_ingredients(dishes, intersection):
     """Determine which `dishes` have a singleton ingredient (an ingredient that only appears once across dishes).
@@ -109,5 +142,12 @@ def singleton_ingredients(dishes, intersection):
 
     The function should return a `set` of ingredients that only appear in a single dish.
     """
+    singleton_set = set()
+    for single_dish in dishes:
+        singleton_set.update(single_dish - intersection)
+    return singleton_set
 
-    pass
+from sets_categories_data import (example_dishes,
+                                  EXAMPLE_INTERSECTION)
+
+print(singleton_ingredients(example_dishes, EXAMPLE_INTERSECTION))
