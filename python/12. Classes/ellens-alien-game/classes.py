e96a7615d6209e6e87113dc3fe2e4830fdcf04a7
siddique-electricaleng <ecsesiddique.297@gmail.com>
2025-01-26 01:34:30 +0600

    Office: Python Classes Intro Completed.

--- python/12. Classes/ellens-alien-game/classes.py
+++ python/12. Classes/ellens-alien-game/classes.py
@@ -18,8 +18,41 @@ class Alien:
     teleport(new_x_coordinate, new_y_coordinate): Move Alien object to new coordinates.
     collision_detection(other): Implementation TBD.
     """
-
-    pass
-
-
 #TODO:  create the new_aliens_collection() function below to call your Alien class with a list of coordinates.
+    # Class attributes
+    total_aliens_created = 0
+    # Constructor function/ Dunder method
+    def __init__(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        self.health = 3
+        Alien.total_aliens_created += 1
+
+    def hit(self):
+        if (self.health > 0):
+            self.health -= 1
+        else:
+            self.health = 0
+        return self.health
+    
+    def is_alive(self):
+        if (self.health > 0):
+            return True
+        else:
+            return False
+        
+    def teleport(self, x_coordinate, y_coordinate):
+        self.x_coordinate = x_coordinate
+        self.y_coordinate = y_coordinate
+        return x_coordinate, y_coordinate
+    
+    def collision_detection(self, other_obj):
+        pass
+
+# Task 7 - Note: You need to see output in the question provided, to understand what the function does and how to make it below
+def new_aliens_collection(alien_locations):
+    alien_objs = list()
+    for alien_location in alien_locations:
+        alien_objs.append(Alien(alien_location[0],alien_location[1]))
+    return alien_objs
+
