73dfe635bd455351c0d65100d47489390b2525c7
siddique-electricaleng <ecsesiddique.297@gmail.com>
2025-02-11 11:09:22 +0600

    Office: Writing to a csv file is complete

--- tim_s_Course_python/8_Reading and writing files in Python/lesson04_csv.py
+++ tim_s_Course_python/8_Reading and writing files in Python/lesson04_csv.py
@@ -9,7 +9,7 @@
 271 - 30) Solution to DictReader Challenge
 """
 import csv
-# 24) Reading in a CSV file
+# 24) Reading a CSV file
 """ 
 csv_filename = 'OlympicMedals_2020.csv'
 
@@ -32,6 +32,7 @@ with open(cereals_filename, encoding='utf-8', newline='') as csv_file:
 """
 
 # 26) Sniffer and Dialect
+""" 
 input_filename = 'country_info.txt'
 
 with open(input_filename, encoding='utf-8', newline='') as countries_data:
@@ -44,9 +45,55 @@ with open(input_filename, encoding='utf-8', newline='') as countries_data:
 
     # Move file pointer to beginning of file using seek() method
     countries_data.seek(0)
+
     country_reader = csv.reader(countries_data, dialect=country_dialect)
     for row in country_reader:
         print(row)
-print('*'*80)
+ """
+# 27) CSV Dialect - seeing what is inside the Dialect Object
+# Using the previous code
+""" 
+print('*' * 80)
+attributes = ['delimiter',
+              'doublequote',
+              'escapechar',
+              'lineterminator',
+              'quotechar',
+              'quoting',
+              'skipinitialspace'
+              ]
 
-# 27) CSV Dialect
+for attribute in attributes:
+    print(f"{attribute}: {repr(getattr(country_dialect, attribute))}")
+ """
+# 28) Writing a CSV File
+""" 
+cereals = [
+    ["Barley", 556, 1.7, 32.9, 10.1, 13.8],
+    ["Durum", 339, 5, 27.4, 4.09, 9.7],
+    ["Fonio", 240, 1, 4, 1.7, 0.05],
+    ["Maize", 442, 7.4, 37.45, 6.15, 11.03],
+    ["Millet", 484, 2, 37.9, 13.4, 9.15],
+    ["Oats", 231, 9.2, 35.1, 10.3, 3.73],
+    ["Rice (Brown)", 346, 2.8, 38.1, 9.9, 0.8],
+    ["Rice, (White)", 345, 3.6, 37.6, 5.4, 0.1],
+    ["Rye", 422, 2, 31.4, 18.2, 21.2],
+    ["Sorghum", 316, 3, 37.8, 9.92, 9.15],
+    ["Triticale", 338, 1.81, 36.6, 19, 0.9],
+    ["Wheat", 407, 1.2, 27.8, 12.9, 13.8],
+]
+
+column_headings = ["Cereal", "Calories",
+                   "Fat", "Protein", "Fibre", "Vitamin E"]
+
+output_filename = 'my_cereals.csv'
+
+with open(output_filename, 'w', encoding='utf-8', newline='') as output_file:
+    # Create the writer object
+    writer = csv.writer(output_file, quoting=csv.QUOTE_NONNUMERIC)
+    # Write the column heading first
+    writer.writerow(column_headings)
+    # Use the writer object to write each row 1 at a time
+    writer.writerows(cereals)
+"""
+# 29) The csv DictReader
