63f46d2bc1dc0302f36626a7c8eb4837bfce9320
siddique-electricaleng <ecsesiddique.297@gmail.com>
2025-02-18 15:38:48 +0600

    Office: Completed Duck typing from Bro Code

--- Bro Code - OOP specifically/7_duck_typing.py
+++ Bro Code - OOP specifically/7_duck_typing.py
@@ -4,3 +4,38 @@ Another way to achieve polymorphism besides Inheritance
 Object must have the minimum necessary attributes/methods
 If it looks like a duck and quacks like a duck. it must be a duck.
 """
+
+
+class Animal:
+    alive = True
+
+
+class Dog(Animal):
+    def speak(self):
+        print("Woof")
+
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW")
+
+# Here car used duck typing to achieve polymorphism:
+# Recall: If it looks like a duck & quacks like a duck - it must be a duck
+# Meaning, car has the necessary attributes and methods to say that it is an Animal, just like Dog and Cat
+# Therefore, Car has the forms:
+# 1. Car
+# 2. Animal
+
+
+class Car:
+    alive = False
+
+    def speak(self):
+        print("Honk")
+
+
+animals = [Dog(), Cat(), Car()]
+
+for animal in animals:
+    animal.speak()
+    print(animal.alive)
