945a12db33bdc94888385e31549185e7b239388e
siddique-electricaleng <ecsesiddique.297@gmail.com>
2025-02-19 18:06:29 +0600

    Office: Slight cleanup and refactoring of system path for reading and writing to files (done inside lesson03.py where we worked with JSON)

--- tim_s_Course_python/8_Reading and writing files in Python/lesson03_JSON.py
+++ tim_s_Course_python/8_Reading and writing files in Python/lesson03_JSON.py
@@ -22,11 +22,11 @@ languages = [
 # Writing the data
 with open('test.json', 'w', encoding='utf-8') as testfile:
     json.dump(languages, testfile)
- """
+"""
 # Reading the data back from the test.json file
 """ 
-dir_path = "." +sys.path[0].removeprefix(r'c:\Users\Abdul-Hamid\Exercism')
-file_path = dir_path + (r"\test.json")
+dir_path = "." +sys.path[0].removeprefix('c:\\Users\\Abdul-Hamid\\Exercism')
+file_path = dir_path + ("\\test.json")
 with open(file_path, 'r', encoding='utf-8') as testfile:
     data = json.load(testfile)
 print(data)
@@ -34,16 +34,17 @@ print(data[2])
 """
 # 22) Practical application parsing JSON data
 """ 
-json_data_source = 'temperature_anomaly.json'
+dir_path = "." +sys.path[0].removeprefix('c:\\Users\\Abdul-Hamid\\Exercism')
+json_data_source = dir_path + '\\temperature_anomaly.json'
 with open(json_data_source, 'r', encoding='utf-8') as json_temp_data:
     anomalies = json.load(json_temp_data)
 
 for year, value in anomalies['data'].items():
     year, value = int(year), float(value)
     print(f"{year}...{value:6.2f}")
-"""
+ """
 # 23) Practical application parsing JSON data from the internet
-""" 
+
 url_data_source_json = 'https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/global/time-series/globe/tavg/land_ocean/1/12/1850-2024/data.json'
 
 # download json data from the internet
@@ -52,9 +53,8 @@ with urllib.request.urlopen(url_data_source_json) as json_stream:
     data = json_stream.read().decode('UTF-8')
     anomalies = json.loads(data)
 
-print(type(anomalies))
+# print(anomalies)
 #  printing the year....value
 
 for year in anomalies['data']:
     print(f"{year}...{anomalies['data'][str(year)]['anomaly']}")
- """
