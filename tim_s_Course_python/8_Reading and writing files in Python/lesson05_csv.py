87830dcea04c568dae6243ef32682bcf758935b4
siddique-electricaleng <ecsesiddique.297@gmail.com>
2025-02-19 19:44:06 +0600

    Office: Could not resume this anymore. My head hurts and so does my heart. I have been awake for too long.

--- tim_s_Course_python/8_Reading and writing files in Python/lesson05_csv.py
+++ tim_s_Course_python/8_Reading and writing files in Python/lesson05_csv.py
@@ -7,3 +7,24 @@
 35. Solution to parsing functions challenge
 """
 
+import csv
+import sys
+dir_path = "." + sys.path[0].removeprefix('c:\\Users\\Abdul-Hamid\\Exercism')
+country_filename = dir_path + '\\country_info.txt'
+dialect = csv.excel
+dialect.delimiter = '|'
+
+countries = {}
+with open(country_filename, encoding='utf-8', newline='') as countries_file:
+    # Get the column headings from the 
+    reader = csv.DictReader(countries_file, dialect = dialect)
+
+    for row in reader:
+        countries[row['Country'].casefold()] = row
+        countries[row['CC'].casefold()] = row
+
+user_test = input(
+    "Enter the Country or 2 letter coutnry code to see it's capital: ").casefold()
+print(
+    f"The capital of {countries[user_test]['Country']} is {countries[user_test]['Capital']}\r")
+ 
