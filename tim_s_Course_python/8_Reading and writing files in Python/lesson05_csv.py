64a78143e1e1e45adff4ded6a06d675d9c04dc6e
siddique-electricaleng <ecsesiddique.297@gmail.com>
2025-02-19 18:47:50 +0600

    Office: Added all python script files for writing code.

---
+++ tim_s_Course_python/8_Reading and writing files in Python/lesson05_csv.py
@@ -0,0 +1,9 @@
+"""
+30. Field names with DictReader and DictWriter
+31. Reading and writing multiple files
+32. The csv DictWriter
+33. The zip function
+34. Reading and writing to the same text file
+35. Solution to parsing functions challenge
+"""
+
---
+++ tim_s_Course_python/8_Reading and writing files in Python/lesson07_binary_data.py
@@ -0,0 +1,14 @@
+"""
+40. Working with binary files bytes & bytearray
+41. Reading a bitmap
+42. Little endian & big endian
+43. Making sense of binary data
+44. Reading tags in an mp3 file
+45. The ID3v2 specification
+46. The code
+47. Filling in the blanks
+48. Extracting images
+49. Testing our readid3 program
+50. Checking the hash of a file
+51. End of Remaster
+"""
