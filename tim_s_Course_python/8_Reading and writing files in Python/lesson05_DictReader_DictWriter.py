51a000edb3c84dcd801551ed9982fc258ba7a68b
siddique-electricaleng <ecsesiddique.297@gmail.com>
2025-02-20 17:33:36 +0600

    Office : Solution to parsing functions challenge completed.

--- tim_s_Course_python/8_Reading and writing files in Python/lesson05_DictReader_DictWriter.py
+++ tim_s_Course_python/8_Reading and writing files in Python/lesson05_DictReader_DictWriter.py
@@ -77,25 +77,112 @@ with open((dir_path+'\OlympicMedals_2020.csv'), encoding='utf-8', newline='') as
 
 # 33. The zip function
 # This is a list of tuples for song albums
+""" 
 albums = [
     ("Welcome to my Nightmare", "Alice Cooper", 1975),
     ("Bad Company", "Bad Company", 1974),
     ("Nightflight", "Budgie", 1981),
     ("More Mayhem", "Imelda May", 2011),
-    ("Ride the Lightning", "Metallica", 1984),
+    ("Ride the Lightning", "Metallica", 1984)
 ]
 # We want to use DictWriter to write this info into a csv file
 keys = ['album', 'artist', 'year']
+
 remove_str = os.path.expanduser("~") + "\\Exercism"
 dir_path = ".\\" + sys.path[0].strip(remove_str)
 file_path = dir_path + "\\albums.csv"
-print(remove_str)
-for row in albums:
-    zip_object = zip(keys, row)
-    # zip object is an iterable producing exactly a tuple with 2 elements.
-    # Since there are 3 keys, therefore for each row there will be 3 such tuples containing the key and value pairs
 
-    # We can pass this zip object which is an iterable into the dict() function to convert to a dictionary
-    albums_dict = dict(zip_object)
-    with open(file_path, 'w', encoding='utf-8', newline='') as output_album_file:
-        writer = csv.DictWriter(output_album_file)
+
+with open(file_path, 'w', encoding='utf-8', newline='') as output_album_file:
+    writer = csv.DictWriter(output_album_file, fieldnames=keys)
+    writer.writeheader()
+
+    # Convert the 2 lists into a list of tuples using zip function
+    for row in albums:
+        zip_object = zip(keys, row)
+        # convert zip object or the (key,value) tuples into a dictionary for DictWriter to write to a csv
+        albums_dict = dict(zip_object)
+        # Since we are inside the loop. We must write each row individually to the csv and not altogether
+        writer.writerow(albums_dict)
+ """
+# 34. Reading and writing to the same text file  & parsing 
+# Code already provided semi-completed.
+# We did the code for parse_invoice_number & next_invoice_number functions
+
+import datetime
+from os import SEEK_SET
+from typing import TextIO
+
+
+def get_year() -> int:
+    """Return the current year as an integer."""
+    return datetime.datetime.now().year
+
+
+def parse_invoice_number(invoice_number: str) -> tuple[int, int]:
+    # TODO1: Completed
+    """Split a well-formed invoice "number" into its component parts.
+
+    :param invoice_number: A string of the form YYYY-NNNN
+        YYYY is the 4 digit year.
+        NNNN is a 4 digit invoice number, left padded with zeros.
+        The 2 parts are separated with a "-" character.
+    :return: The returned tuple will contain:
+        the 4 digit year as an integer,
+        the invoice number as an integer.
+    """
+    return tuple(map(int, invoice_number.split('-')))
+
+
+def next_invoice_number(invoice_number: str) -> str:
+    # TODO2: Ongoing
+    """ Produce the next invoice "number" in sequence.
+
+    The format of `invoice_number` is described in `parse_invoice_number`.
+
+    :param invoice_number: A string representing an invoice number.
+    :return: A string representing the next invoice number.
+        The numerical part will be incremented, unless the current year
+        isn't the same as the year in `invoice_number`. In that case,
+        the new invoice number will contain the current year, and the
+        numerical part will be set to "0001".
+    """
+    year, prev_inv_num = parse_invoice_number(invoice_number)
+    return f"{get_year()}-{str(1 if (year!= get_year()) else (prev_inv_num + 1)).rjust(4,'0')}"
+
+def record_invoice(invoice_file: TextIO,
+                   company: str,
+                   amount: float) -> None:
+    # TODO3: Future videos
+    """Create a new invoice number, and write it to a file on disk.
+
+    :param invoice_file: An open text file, opened using r+.
+    :param company: The name of the company being invoiced.
+    :param amount: The amount of the invoice.
+    """
+
+
+# Test code:
+current_year = get_year()
+test_data = [
+    ('2019-0005', (2019, 5), f'{current_year}-0001'),
+    (f'{current_year}-8514', (current_year, 8514), f'{current_year}-8515'),
+    (f'{current_year}-0001', (current_year, 1), f'{current_year}-0002'),
+    (f'{current_year}-0023', (current_year, 23), f'{current_year}-0024'),
+]
+
+for test_string, result, next_number in test_data:
+    parts = parse_invoice_number(test_string)
+    if parts == result:
+        print(f'{test_string} parsed successfully')
+    else:
+        print(f'{test_string} failed to parse. Expected {result} got {parts}')
+
+    new_number = next_invoice_number(test_string)
+    if next_number == new_number:
+        print(f'New number {new_number} generated correctly for {test_string}')
+    else:
+        print(f'New number {new_number} is not correct for {test_string}')
+
+    print('-' * 80)
+
