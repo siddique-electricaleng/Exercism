25b22d5d881bb61f571aa3b7c26d0635bcc416af
Abu Bakr Siddique <ecsesiddique.297@gmail.com>
2025-02-20 06:29:14 +0600

    Home: Only used fieldnames with DictReader

--- tim_s_Course_python/8_Reading and writing files in Python/lesson05_csv.py
+++ tim_s_Course_python/8_Reading and writing files in Python/lesson05_csv.py
@@ -6,25 +6,34 @@
 34. Reading and writing to the same text file
 35. Solution to parsing functions challenge
 """
-
+# 30) Field names with DictReader (he didn't show DictWriter yet)
+""" 
 import csv
 import sys
-dir_path = "." + sys.path[0].removeprefix('c:\\Users\\Abdul-Hamid\\Exercism')
+dir_path = "." + sys.path[0].removeprefix('c:\\Users\\Acer\\Exercism')
 country_filename = dir_path + '\\country_info.txt'
 dialect = csv.excel
 dialect.delimiter = '|'
 
 countries = {}
 with open(country_filename, encoding='utf-8', newline='') as countries_file:
-    # Get the column headings from the 
-    reader = csv.DictReader(countries_file, dialect = dialect)
+    # Get the column headings from the file and make it lowercase
+    headings = countries_file.readline().strip('\n').split(dialect.delimiter)
+    for index, heading in enumerate(headings):
+        headings[index] = heading.casefold()
+    
+
+    reader = csv.DictReader(countries_file, dialect = dialect, fieldnames=headings)
 
     for row in reader:
-        countries[row['Country'].casefold()] = row
-        countries[row['CC'].casefold()] = row
+        countries[row['country'].casefold()] = row
+        countries[row['cc'].casefold()] = row
 
 user_test = input(
-    "Enter the Country or 2 letter coutnry code to see it's capital: ").casefold()
+    "Enter the Country or 2 letter country code to see it's capital: ").casefold()
 print(
-    f"The capital of {countries[user_test]['Country']} is {countries[user_test]['Capital']}\r")
- 
+    f"The capital of {countries[user_test]['country']} is {countries[user_test]['capital']}\r")
+"""
+
+# Reading and Writing multiple files at the same time - continue from 1.30
+
